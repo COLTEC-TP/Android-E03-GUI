6eecd5b5c9805187f6c4704cb04ac1b6809d9ffc
ritarez <ritarezborges@gmail.com>
2018-03-15 17:12:42 -0300

    Alert Dialogs done and string content added

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/Form.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/Form.java
@@ -1,6 +1,8 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +11,38 @@ import android.widget.Toast;
 
 public class Form extends Activity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setIcon(R.drawable.ic_launcher_foreground);
+        alertBuilder.setTitle(R.string.alertTitle);
+        alertBuilder.setMessage(R.string.alertMessage);
+
+        alertBuilder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getBaseContext(), R.string.negativeButton, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertBuilder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getBaseContext(), R.string.positiveButton, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         final Button button = findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(Form.this, "Your anwser has been registered", Toast.LENGTH_SHORT).show();
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
             }
 
         });
