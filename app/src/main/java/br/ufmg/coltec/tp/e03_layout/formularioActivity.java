3fca8c60781504320f447390bb915b244e6a7f58
kingnowdo <reinaldomagas@gmail.com>
2018-03-21 14:24:01 -0300

    Terminei Utilizando Dialogs

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/formularioActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/formularioActivity.java
@@ -1,7 +1,12 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class formularioActivity extends Activity {
 
@@ -9,5 +14,35 @@ public class formularioActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+        alertBuilder.setTitle("Espere!");
+        alertBuilder.setMessage("Você quer mesmo enviar?");
+
+        alertBuilder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getBaseContext(), "Comentário enviado", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getBaseContext(), "O que está esperando? Envie seu comentário!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final AlertDialog dialog = alertBuilder.create();
+
+        Button bEnviar = findViewById(R.id.botao);
+
+        bEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
     }
 }
