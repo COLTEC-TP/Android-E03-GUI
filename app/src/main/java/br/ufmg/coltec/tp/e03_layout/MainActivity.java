e82e67c8946942a09c945d5b5059ab28191038bc
mathias goncalves oliveira <a2016952827@la0012217531.infocoltec.local>
2018-03-22 11:02:34 -0300

    exercicios widgets

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -1,7 +1,10 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,42 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button splash = findViewById(R.id.Splash);
+        Button noticia = findViewById(R.id.noticia);
+        Button tempo = findViewById(R.id.tempo);
+        Button form = findViewById(R.id.form);
+
+        splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FrameLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        noticia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Noticia.class);
+                startActivity(intent);
+            }
+        });
+
+        tempo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, wheather.class);
+                startActivity(intent);
+            }
+        });
+
+        form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, form.class);
+                startActivity(intent);
+            }
+        });
     }
 }
