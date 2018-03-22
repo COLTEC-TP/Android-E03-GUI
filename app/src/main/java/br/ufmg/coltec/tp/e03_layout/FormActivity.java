5e55dcb6f83bfcecd7a7af487accb952aa48f946
haasilva <haasilva@yahoo.com.br>
2018-03-22 14:41:33 -0300

    Entrega Widgets

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/FormActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/FormActivity.java
@@ -1,8 +1,8 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,13 +14,31 @@ public class FormActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        Button enviar = findViewById(R.id.submit);
 
-        Button enviar = (Button) findViewById(R.id.submit);
+        alertBuilder.setIcon(R.drawable.ic_launcher_foreground);
+        alertBuilder.setTitle(getResources().getText(R.string.send));
+        alertBuilder.setMessage(getResources().getText(R.string.sure));
+        alertBuilder.setPositiveButton(getResources().getText(R.string.sim), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getBaseContext(), getResources().getText(R.string.enviado), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertBuilder.setNegativeButton(getResources().getText(R.string.nao), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getBaseContext(), getResources().getText(R.string.voltando), Toast.LENGTH_LONG).show();
+            }
+        });
 
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(FormActivity.this, "Dados Enviados", Toast.LENGTH_LONG).show();
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
             }
             });
         }
--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -13,9 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button noticia = (Button) findViewById(R.id.button);
-        Button previsao = (Button) findViewById(R.id.button2);
-        Button formulario = (Button) findViewById(R.id.button3);
+        Button noticia = findViewById(R.id.button);
+        Button previsao = findViewById(R.id.button2);
+        Button formulario = findViewById(R.id.button3);
 
         noticia.setOnClickListener(new View.OnClickListener() {
             @Override
