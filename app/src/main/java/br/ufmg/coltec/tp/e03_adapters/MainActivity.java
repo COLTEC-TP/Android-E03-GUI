d2d64bcb4916f913d08bce096b5892866b043993
RitaRez <ritarezborges@gmail.com>
2018-03-29 10:30:17 -0300

    I tried...

--- app/src/main/java/br/ufmg/coltec/tp/e03_adapters/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_adapters/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.widget.ListView lvPlaces = findViewById(R.id.place_list);
+        lvPlaces.setAdapter(new PlaceAdapter(this));
     }
 }
