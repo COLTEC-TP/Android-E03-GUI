4aee36807f17e512b9ba8dba45dcf6ac7748716b
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-06-21 03:26:59 -0300

    Frame Layout

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -1,7 +1,9 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button frameLayout = findViewById(R.id.buttonFrameLayout);
+
+        frameLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, FrameLayout.class);
+            startActivity(intent);
+        });
+
     }
 }
