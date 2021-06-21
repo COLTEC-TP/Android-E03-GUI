c691ef11ec52710ddb6ecf969f36ecaf58b27f02
Frederico Teixeira Azevedo <2020955533@teiacoltec.org>
2021-06-21 02:21:44 -0300

    commit_Splash_Screen
    Commit da tarefa E03-GUI-layouts-SplashScreen

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/SplashActivity.java
@@ -0,0 +1,30 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
