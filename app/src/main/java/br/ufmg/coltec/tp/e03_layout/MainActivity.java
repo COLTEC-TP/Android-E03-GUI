0d6685d8640e336a18786b0cbd94d0513663645e
Jeffersonabreu924 <2020954936@teiacoltec.org>
2021-06-23 16:53:04 -0300

    Layouts - Jefferson Abreu 2020954936

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -1,13 +1,34 @@
 package br.ufmg.coltec.tp.e03_layout;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button noticia = this.findViewById(R.id.btn_noticia);
+        Button previsaoTempo = this.findViewById(R.id.btn_previsao_tempo);
+        Button formulario = this.findViewById(R.id.btn_formulario);
+
+        noticia.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NoticiaActivity.class);
+            startActivity(intent);
+        });
+        previsaoTempo.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PrevisaoTempoActivity.class);
+            startActivity(intent);
+        });
+        formulario.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, FormularioActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/NoticiaActivity.java
@@ -0,0 +1,13 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NoticiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticia);
+    }
+}
