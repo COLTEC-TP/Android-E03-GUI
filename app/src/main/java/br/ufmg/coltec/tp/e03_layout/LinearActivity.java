0450ec6be449fe4fce20eeb4f7f4f8ea530e1166
DougNaves <61755699+DougNaves@users.noreply.github.com>
2021-06-23 20:10:58 -0300

    Layouts - Douglas Coelho

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/LinearActivity.java
@@ -0,0 +1,14 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
