95976a0ecd2d624c11f9a18e376888f2947ca5d4
DougNaves <61755699+DougNaves@users.noreply.github.com>
2021-06-23 18:10:54 -0300

    Entrega da atividade - Douglas Coelho

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/MainActivity.java
@@ -1,13 +1,44 @@
 package br.ufmg.coltec.tp.e03_layout;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    Button btnFrame, btnRelative, btnTable, btnLinear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFrame = findViewById(R.id.btnFrame);
+        btnRelative = findViewById(R.id.btnRelative);
+        btnTable = findViewById(R.id.btnTable);
+        btnLinear = findViewById(R.id.btnLinear);
+
     }
+
+    public void frameActivity (View view){
+        Intent intent = new Intent (MainActivity.this, FrameActivity.class);
+        startActivity(intent);
+    }
+    public void relativeActivity (View view){
+        Intent intent = new Intent (MainActivity.this, RelativeActivity.class);
+        startActivity(intent);
+    }
+    public void tableActivity (View view){
+        Intent intent = new Intent (MainActivity.this, TableActivity.class);
+        startActivity(intent);
+    }
+    public void linearActivity (View view){
+        Intent intent = new Intent (MainActivity.this, LinearActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
