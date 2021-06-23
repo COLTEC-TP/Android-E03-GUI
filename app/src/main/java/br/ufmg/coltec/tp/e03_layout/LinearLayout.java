da3ef5030b9e38aa7b29f99ece25be6fa432cc55
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-06-23 16:02:49 -0300

    Dialog

--- app/src/main/java/br/ufmg/coltec/tp/e03_layout/LinearLayout.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/LinearLayout.java
@@ -1,7 +1,11 @@
 package br.ufmg.coltec.tp.e03_layout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LinearLayout extends Activity {
 
@@ -9,5 +13,31 @@ public class LinearLayout extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
+
+        Button buttonSend = findViewById(R.id.btnSend);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmação");
+        builder.setMessage("Você tem certeza que quer enviar o formulário?");
+
+        builder.setPositiveButton("Concordo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(LinearLayout.this, "Envio confirmado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Discordo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(LinearLayout.this, "Envio cancelado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+
+        buttonSend.setOnClickListener(view -> {
+            dialog.show();
+        });
+
     }
 }
