23444d4f305005f973fbbeb5a1d4c00d1949d6f9
Jeffersonabreu924 <2020954936@teiacoltec.org>
2021-06-25 11:23:17 -0300

    Widgets - Jefferson Abreu 2020954936

---
+++ app/src/main/java/br/ufmg/coltec/tp/e03_layout/PrevisaoTempoActivity.java
@@ -0,0 +1,13 @@
+package br.ufmg.coltec.tp.e03_layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PrevisaoTempoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previsao_tempo);
+    }
+}
