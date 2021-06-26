4cfe12d99274c2e587433228179ac76a5252b3c3
Nandaoc <59846823+Nandaoc@users.noreply.github.com>
2021-06-26 17:50:58 -0300

    Criando adapter

--- app/src/main/java/br/ufmg/coltec/tp/e03_adapters/Place.java
+++ app/src/main/java/br/ufmg/coltec/tp/e03_adapters/Place.java
@@ -1,5 +1,16 @@
 package br.ufmg.coltec.tp.e03_adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by jemaf on 07/03/18.
  */
@@ -7,8 +18,8 @@ public class Place {
 
     private String name;        // nome do local
     private Integer photoId;    // id da foto do local
-    private Double distance;    // distância (em km)
-    private Double rate;        // nota (1 a 5)
+    private Integer distance;    // distância (em km)
+    private Float rate;        // nota (1 a 5)
 
     /**
      * Construtor
@@ -17,7 +28,7 @@ public class Place {
      * @param distance Distância do local em KM
      * @param rate Nota do local (1 a 5)
      */
-    public Place(String name, Integer photoId, Double distance, Double rate) {
+    public Place(String name, Integer photoId, Integer distance, Float rate) {
         this.name = name;
         this.photoId = photoId;
         this.distance = distance;
@@ -47,19 +58,69 @@ public class Place {
         this.photoId = photoId;
     }
 
-    public Double getDistance() {
+    public Integer getDistance() {
         return distance;
     }
 
-    public void setDistance(Double distance) {
+    public void setDistance(Integer distance) {
         this.distance = distance;
     }
 
-    public Double getRate() {
+    public Float getRate() {
         return rate;
     }
 
-    public void setRate(Double rate) {
+    public void setRate(Float rate) {
         this.rate = rate;
     }
+
+    public static class PlaceAdapter extends BaseAdapter{
+
+        private ArrayList<Place> places;
+        private Context context;
+
+        public PlaceAdapter(Context context){
+            this.context = context;
+            places = new ArrayList<>();
+        }
+
+        @Override
+        public int getCount() {
+            return this.places.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return this.places.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Place place = this.places.get(i);
+
+            View newView = LayoutInflater.from(this.context).inflate(R.layout.list_item_place, viewGroup, false);
+
+            ImageView photoPlace = newView.findViewById(R.id.img_place_photo);
+            TextView namePlace = newView.findViewById(R.id.img_place_photo);
+            TextView textPlaceName = newView.findViewById(R.id.txt_place_name);
+            RatingBar placeEvaluation = newView.findViewById(R.id.txt_place_name);
+            TextView placeLocation = newView.findViewById(R.id.img_place_photo);
+
+            photoPlace.setImageResource(place.getPhotoId());
+            namePlace.setText(place.getName());
+            textPlaceName.setText(place.getName());
+            placeEvaluation.setRating(place.getRate());
+            placeLocation.setText(place.getDistance());
+
+            return newView;
+        }
+    }
 }
+
+
+
